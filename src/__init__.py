17a4468670f30f465238cf950bb560b74d55b96f
Espy | Neko | 404 <34942042+neko404notfound@users.noreply.github.com>
2018-05-22 21:50:41 +0100

    Create __init__.py

---
+++ src/__init__.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3.6
+"""
+Sebi-Machine.
+"""
+
+__author__ = 'Annihilator708'
+__license__ = 'MIT'
+__title__ = 'Sebi-Machine'
+__version__ = 'tbd'
+
+__repository__ = f'https://github.com/{__author__}/{__title__}'
+__url__ = __repository__
